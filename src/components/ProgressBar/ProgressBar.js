8f587640a63f623d8a48822ca381f57eb086f72d
Michael Caveney <dylanesque27@gmail.com>
2021-03-25 22:25:07 -0400

    Added ProgressBar Code

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,68 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
+const STYLES = {
+  small: {
+    height: 8,
+    padding: 0,
+    radius: 4,
+  },
+  medium: {
+    height: 12,
+    padding: 0,
+    radius: 4,
+  },
+  large: {
+    height: 16,
+    padding: 4,
+    radius: 8,
+  },
+};
+
+const Bar = styled.div`
+  width: var(--width);
+  height: var(--height);
+  height: 8px;
+  background-color: ${COLORS.primary};
+  border-radius: 4px 0 0 4px;
+`;
+
+const Wrapper = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  border-radius: var(--radius);
+  padding: var(--padding);
+`;
+
+const BarWrapper = styled.div`
+  border-radius: 4px;
+  /* This is necessary to prevent the corners of the child from protruding at very high or very low values */
+  overflow: hidden;
+`;
+
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  const styles = STYLES[size];
+
+  if (!styles) {
+    throw new Error(`Unknown size passed to ProgressBar: ${size}`);
+  }
+  return (
+    <Wrapper
+      role="progressbar"
+      aria-valuenow={value}
+      aria-valuemin="0"
+      aria-valuemax="100"
+      style={{
+        '--padding': styles.padding + 'px',
+        '--radius': styles.radius + 'px',
+      }}
+    >
+      <VisuallyHidden>{value}</VisuallyHidden>
+      <Bar
+        style={{ '--width': value + '%', '--height': styles.height + 'px' }}
+      />
+    </Wrapper>
+  );
 };
 
 export default ProgressBar;
